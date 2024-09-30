8c21514dc80b7a7d797d338c58faecec69671d08
MartaCEI <guecolmar@alumnos.cei.es>
2024-09-30 18:26:41 +0200

    __dirname

--- config/config.js
+++ config/config.js
@@ -6,3 +6,6 @@ export const PORT = process.env.PORT || 5000;
 export const DOMAIN = process.env.DOMAIN || 'http://localhost';
 
 export const URL = `${DOMAIN}:${PORT}`;
+
+import path from 'path';
+export const __dirname = path.resolve();
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import indexRoutes from './routes/index.routes.js';
 import { PORT, DOMAIN, URL } from './config/config.js'
+import path from 'path';
+import { __dirname } from './config/config.js';
 
 const app = express();
 
@@ -12,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public"))) 
 
 app.use("/api/v1", indexRoutes);
 // GET /
